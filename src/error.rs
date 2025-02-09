ecba9fa717423dd3da0456391d68acd34bcfaaa6
Phoenix Angulo <26429095+Kajmany@users.noreply.github.com>
2025-02-08 18:46:41 -0800

    Pass through Json extractor error to response. Collapsed RouteError into an enum from struct w/ enum

--- src/error.rs
+++ src/error.rs
@@ -10,15 +10,18 @@ use axum::{
 };
 use serde::Serialize;
 
+/*
 pub struct RouteError {
     kind: Kind,
     source: Option<Box<dyn std::error::Error + Send + Sync>>,
 }
-enum Kind {
-    BadRequestJson,
+*/
+
+pub enum RouteError {
+    BadRequestJson(Box<JsonRejection>),
     // TODO: See if we need to actually capture a string here or we can trace state outside
     ExternalAPIParse(String),
-    ExternalAPIRequest,
+    ExternalAPIRequest(Box<reqwest::Error>),
 }
 
 impl IntoResponse for RouteError {
@@ -27,16 +30,17 @@ impl IntoResponse for RouteError {
         struct ErrorResponse {
             message: String,
         }
-        let (status, message) = match self.kind {
-            //TODO: Probably pass through whatever message and statsucode for BadReqJson
-            // AKA internal parse failure, because it should be handled by JSON extractor
-            Kind::BadRequestJson => (StatusCode::BAD_REQUEST, "OOPS!".to_owned()),
-            Kind::ExternalAPIParse(_) => (
+        let (status, message) = match self {
+            // The user sent this info, so it should be safe to pass the full error back
+            RouteError::BadRequestJson(err) => (err.status(), err.body_text()),
+            RouteError::ExternalAPIParse(_) => (
                 // Purposely vague. Pretty sure it should be 500 because we're not a gateway?
                 StatusCode::INTERNAL_SERVER_ERROR,
                 "problem parsing external API response".to_owned(),
             ),
-            Kind::ExternalAPIRequest => (StatusCode::INTERNAL_SERVER_ERROR, "OOPS!".to_owned()),
+            RouteError::ExternalAPIRequest(_) => {
+                (StatusCode::INTERNAL_SERVER_ERROR, "OOPS!".to_owned())
+            }
         };
         (status, Json(ErrorResponse { message })).into_response()
     }
@@ -44,30 +48,20 @@ impl IntoResponse for RouteError {
 
 impl RouteError {
     pub fn new_external_parse_failure(msg: String) -> Self {
-        RouteError {
-            kind: Kind::ExternalAPIParse(msg),
-            source: None,
-        }
+        RouteError::ExternalAPIParse(msg)
     }
 }
 
 // TODO: Decide where to tracepoint, probably here. Also, distinguish parse problems
 impl From<reqwest::Error> for RouteError {
-    fn from(value: reqwest::Error) -> Self {
-        RouteError {
-            kind: Kind::ExternalAPIRequest,
-            source: Some(Box::new(value)),
-        }
+    fn from(err: reqwest::Error) -> Self {
+        RouteError::ExternalAPIRequest(Box::new(err))
     }
 }
 
 impl From<axum::extract::rejection::JsonRejection> for RouteError {
-    fn from(_: JsonRejection) -> Self {
-        RouteError {
-            kind: Kind::BadRequestJson,
-            //TODO: Need details and/or tracept here
-            source: None, //Not a proper error?
-        }
+    fn from(rejection: JsonRejection) -> Self {
+        RouteError::BadRequestJson(Box::new(rejection))
     }
 }
 
